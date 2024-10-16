c467ffae1c15f6508c7b6e5d0264dce06ca4734f
François Lenne <francois.lenne@hotmail.fr>
2024-10-16 23:07:43 +0200

    modify the script for deploying

--- deploy_grun.sh
+++ deploy_grun.sh
@@ -1,11 +1,31 @@
 # having the google cloud cli on your computer
 
+# enable the api for the artifact registry
+
+
 # having a dockerfile
 # docker is install and running
 
 
-gcloud services enable run.googleapis.com containerregistry.googleapis.com
 
 
-docker build -t gcr.io/$project_id/$app_name.
-docker push gcr.io/$project_id/$app_name 
+# create a repo in artifact registry
+
+
+
+gcloud artifacts repositories create [NOM_DU_REPO] --repository-format=docker --location=[REGION] --description="[DESCRIPTION]"
+
+
+# Construire l'image docker 
+
+
+docker build -t [REGION]-docker.pkg.dev/[PROJET_ID]/[NOM_DU_REPO]/[NOM_IMAGE]:[TAG] .
+
+
+
+# configurer l'authentification 
+
+
+docker push [REGION]-docker.pkg.dev/[PROJET_ID]/[NOM_DU_REPO]/[NOM_IMAGE]:[TAG]
+
+
