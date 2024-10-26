f854b23f7664dbf81b331c6e6641f8e028e00da6
François Lenne <francois.lenne@hotmail.fr>
2024-10-26 22:32:47 +0200

    modify the config file (note)

--- deploy_grun.sh
+++ deploy_grun.sh
@@ -38,3 +38,37 @@ gcloud run deploy [NOM_SERVICE] \
   --region=[REGION] \
   --port=8080 \
   --allow-unauthenticated
+
+
+
+
+  # create the secrets for google cloud build 
+
+
+
+  # Create and set secrets
+gcloud secrets create LOCATION --data-file=<(echo "europe-west1")
+gcloud secrets create REPOSITORY --data-file=<(echo "my-repository")
+gcloud secrets create IMAGE --data-file=<(echo "my-app")
+gcloud secrets create TAG --data-file=<(echo "latest")
+gcloud secrets create SERVICE_NAME --data-file=<(echo "my-service")
+gcloud secrets create REGION --data-file=<(echo "europe-west1")
+
+
+# give access to the secrets to google cloud build 
+
+
+
+gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)"
+
+
+
+PROJECT_NUMBER=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
+
+
+CLOUD_BUILD_SA="${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com"
+
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:$CLOUD_BUILD_SA" \
+    --role="roles/secretmanager.secretAccessor"
